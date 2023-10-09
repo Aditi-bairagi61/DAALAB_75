60b2f0f3efd8063c1e9a4c792265659469533be8
Aditi-bairagi61 <aditibairagi1456@gmail.com>
2023-10-09 23:18:34 +0530

    Addes A_1.cpp file

--- A_1.cpp
+++ A_1.cpp
@@ -1,26 +1,33 @@
-// A program to find the number of zeros
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-int firstzeroindex(int arr[], int n)
-{
-	for (int i = 0; i < n; i++) {
-		if (arr[i] == 0) {
-			return i;
-		}
-	}
-	return -1;
-}
+
 int main()
 {
-	int arr[] = { 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	int x = firstzeroindex(arr, n);
-	if (x == -1) {
-		cout << "Count of zero is 0" << endl;
-	}
-	else {
-		cout << "count of zero is " << n - x << endl;
-	}
-	return 0;
-}
+    
+   
+    int i,cnt=0,n;
+    cout<<"Enter size of array:";
+    cin>>n;
+     int a[n];
+     cout<<"Enter"<<" "<<n<<" "<<"Elements\n";
 
+    for(i = 0; i < n; i++)
+    {
+    
+         cin >> a[i];
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i]==0)
+        {
+    
+            cnt++;
+        }
+        
+    }
+    cout<<"Zero occured at how many times::"<<" "<<cnt<<" "<<"times";
+   
+
+    return 0;
+}
