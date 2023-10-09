92ca573580ca54ec8824775506c4ac8fa0af8bb0
Aditi-bairagi61 <aditibairagi1456@gmail.com>
2023-10-09 23:38:28 +0530

    Added A-2.cpp file

---
+++ A_2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[]={1,0,0,1,12,78};
+    int i;
+    for(i=0;i<6;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0;i<6;i++)
+    {
+        cout<<"\n"<<a[i];
+    }
+}
